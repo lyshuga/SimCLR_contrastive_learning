0aaa8fd5bf84d1c6d1c9af39f9faed0111771c3e
mykola.liashuha <mykola.liashuha@centralesupelec.fr>
2021-06-23 20:45:37 +0200

    first commit

---
+++ augmentation/transforms.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from augmentation.autoaugment import distort_image_with_randaugment
+from augmentation.simclr_augment import color_distortion
+
+
+def read_images(features):
+    return features['image']
+
+
+def distort_simclr(image):
+    image = tf.cast(image, tf.float32)
+    v1 = color_distortion(image / 255.)
+    v2 = color_distortion(image / 255.)
+    return v1, v2
+
+
+def read_record(record, input_shape):
+    keys_to_features = {
+        "image_raw": tf.io.FixedLenFeature((), tf.string, default_value=""),
+    }
+
+    features = tf.io.parse_single_example(record, keys_to_features)
+
+    image = tf.io.decode_raw(features['image_raw'], tf.uint8)
+
+    # reshape input and annotation images
+    image = tf.reshape(image, input_shape, name="image_reshape")
+    return image
+
+
+def distort_with_rand_aug(image):
+    image = tf.cast(image, dtype=tf.uint8)
+    v1 = distort_image_with_randaugment(image, num_layers=3, magnitude=5)
+    v2 = distort_image_with_randaugment(image, num_layers=3, magnitude=5)
+    return tf.cast(v1, tf.float32) / 255., tf.cast(v2, tf.float32) / 255.
---
+++ slurm_submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=clr
+#SBATCH --output=%x_%j.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --mem=670000
+#SBATCH --exclude=ruche-gpu01
+
+# Load modules
+unset LD_LIBRARY_PATH
+module load anaconda3/2020.02/gcc-9.2.0
+module load cuda/10.1.243/intel-19.0.3.199
+
+# Activates anaconda environment
+source activate pcamenv
+
+# Links the cudnn library from within the conda envs
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.conda/envs/pcamenv/lib:$HOME/usr/lib64/
+
+# execution
+python3 -u main.py
